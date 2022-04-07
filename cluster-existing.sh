7ef3ecc806f413e08230fefd3dba49c286c4b7fd
Mclover45 <roma-6134@mail.ru>
2022-04-07 16:54:44 +0300

    update

--- cluster-existing.sh
+++ cluster-existing.sh
@@ -1,8 +1,20 @@
 StateExisting=$(aws s3 ls s3://rbachkarou-kops | grep -o  wordsmith.rbachkarou.tl.scntl.com) 
     if [ $StateExisting == "wordsmith.rbachkarou.tl.scntl.com" ] 
     then 
-        echo "yes" 
+        echo "Cluster already there" 
     else 
+    # Create cluster
+    kops create cluster --name=wordsmith.rbachkarou.tl.scntl.com --state=s3://rbachkarou-kops --zones=eu-central-1a --node-count=2 --node-size=t3.medium --master-size=t3.medium
+    kops update cluster --name wordsmith.rbachkarou.tl.scntl.com --yes --admin --state=s3://rbachkarou-kops
     kops validate cluster --wait 10m --state=s3://rbachkarou-kops
+   # Istio install 
+    istioctl install --set profile=minimal -y
+   # Labeled for istio
+    kubectl create namespace wordsmith
+    kubectl label namespace wordsmith istio-injection=enabled
+   # install grafana and kiali and prometeus
+    kubectl apply -f /drone/src/istio-1.13.2/samples/addons/kiali.yaml
+    kubectl apply -f /drone/src/istio-1.13.2/samples/addons/grafana.yaml
+    kubectl apply -f /drone/src/istio-1.13.2/samples/addons/prometheus.yaml
     fi
-
+  
