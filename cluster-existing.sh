de8f35c5615bad9ee3c216ee1d7cda430f40d972
Mclover45 <roma-6134@mail.ru>
2022-04-08 00:11:43 +0300

    commit

--- cluster-existing.sh
+++ cluster-existing.sh
@@ -7,9 +7,5 @@ else
     kops create cluster --name=wordsmith.rbachkarou.tl.scntl.com --state=s3://rbachkarou-kops --zones=eu-central-1a --node-count=2 --node-size=t3.medium --master-size=t3.medium
     kops update cluster --name wordsmith.rbachkarou.tl.scntl.com --yes --admin --state=s3://rbachkarou-kops
     kops validate cluster --wait 10m --state=s3://rbachkarou-kops
-    # Istio install 
-    istioctl install --set profile=minimal -y
-    # ingress install
-    kubectl apply -f ingress/ic-nginx-lb.yaml
 fi
   
