33886bfd2a53f58a6a3639c66c817b173177f8a6
Mclover45 <roma-6134@mail.ru>
2022-04-11 13:16:11 +0300

    script update

--- cluster-existing.sh
+++ cluster-existing.sh
@@ -1,11 +1,11 @@
-    StateExisting=$(aws s3 ls s3://$1 | grep -o  $2) 
+    StateExisting=$(aws s3 ls $1 | grep -o  $2) 
 if [ "$StateExisting" = "$2" ] 
 then 
     echo "Cluster already there" 
 else 
     # Create cluster
-    kops create cluster --name=$2 --state=s3://$1 --zones=eu-central-1a --node-count=2 --node-size=t3.medium --master-size=t3.medium
-    kops update cluster --name $2 --yes --admin --state=s3://$1
-    kops validate cluster --wait 10m --state=s3://$1
+    kops create cluster --name=$2 --state=$1 --zones=eu-central-1a --node-count=2 --node-size=t3.medium --master-size=t3.medium
+    kops update cluster --name $2 --yes --admin --state=$1
+    kops validate cluster --wait 10m --state=$1
 fi
   
