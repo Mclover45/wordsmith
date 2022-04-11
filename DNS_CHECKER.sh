3c1be2c6f15f20316b5cd15146cafaafea8f5114
Mclover45 <roma-6134@mail.ru>
2022-04-11 14:16:48 +0300

    SS

--- DNS_CHECKER.sh
+++ DNS_CHECKER.sh
@@ -1,8 +1,8 @@
-echo '$HOST_NAME_KUBE'
-until [ $HOST_NAME_KUBE -z ]
+HOST_NAME_KUBE=$@
+until ["$HOST_NAME_KUBE" -n]
 do
 HOST_NAME_KUBE=$(kubectl -n ingress-nginx get svc ingress-nginx-controller  -o json | jq .status.loadBalancer.ingress[].hostname)
-echo '$HOST_NAME_KUBE'
-sleep 10
+echo $HOST_NAME_KUBE
+sleep 1
 done
 
