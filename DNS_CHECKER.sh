67489301536f24b451486432996971ffc30c159b
Mclover45 <roma-6134@mail.ru>
2022-04-26 15:09:42 +0300

    set +e add

--- DNS_CHECKER.sh
+++ DNS_CHECKER.sh
@@ -1,7 +1,9 @@
 HOST_NAME_KUBE=""
+set +e
 while [ -z "$HOST_NAME_KUBE"] 
 do
     HOST_NAME_KUBE=$(kubectl -n ingress-nginx get svc ingress-nginx-controller  -o json | jq .status.loadBalancer.ingress[].hostname)
     echo $HOST_NAME_KUBE
-    sleep 30
+    sleep 1
 done
+set -e
--- dns_checker.py
+++ dns_checker.py
@@ -6,4 +6,4 @@ config.load_kube_config("~/.kube/config")   # I'm using file named "config" in t
 v1 = kubernetes.client.CoreV1Api()
 ret = v1.list_service_for_all_namespaces(watch=False)
 for i in ret.items:
-    print(i.status.load_balancer.ingress.hostname)
+    print(i.status.load_balancer)
