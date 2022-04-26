7c539469b304f3e9cdb1791a8318e88e4429310d
Mclover45 <roma-6134@mail.ru>
2022-04-26 17:06:42 +0300

    update new rules

--- dns_checker.py
+++
@@ -1,9 +0,0 @@
-import kubernetes.client
-from kubernetes import client, config
-
-config.load_kube_config("~/.kube/config")   # I'm using file named "config" in the "/root" directory
-
-v1 = kubernetes.client.CoreV1Api()
-ret = v1.list_service_for_all_namespaces(watch=False)
-for i in ret.items:
-    print(i.status.load_balancer)
